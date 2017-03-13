8f9231486016d0ff87838f810d15ebb7627af5c1
SJCaldwell <shane.caldwell12@ncf.edu>
2017-03-13 10:52:28 -0400

    Storing emails in localhost db - need to switch out for mongolab instance

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
-var app     = express();
+var app = express();
 var path    = require('path');
+var expressSanitizer = require('express-sanitizer');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose'); //for working with database
+var Email = require('./models/email');
 var port    = process.env.PORT || 8080 // setting port
 var morgan  = require('morgan'); //for logging
 
+mongoose.connect('mongodb://localhost/srq_science')
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(expressSanitizer()); //blocking stored XSS
 
 app.use(morgan('dev'));
 //set the public folder to server public assets
@@ -20,10 +25,19 @@ app.get('*', function(req, res){
 app.post('*', function(req, res){
   //this can only include the mailing list info
   if (req.body.email && req.body.fname){
-    //log in DB
-    
-  }
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+      req.body.email = req.sanitize(req.body.email);
+      req.body.fname = req.sanitize(req.body.fname);
+      var email = new Email();
+      email.email = req.body.email;
+      email.name = req.body.fname;
+
+      email.save(function(err) {
+        if (err){
+          return res.send(err);
+        }
+      });
+    }
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(8080);
