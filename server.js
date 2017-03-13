c2281a9ab220e041e81e80dac64304ff717af0a1
SJCaldwell <shane.caldwell12@ncf.edu>
2017-03-13 09:49:33 -0400

    Updated index.html to send POST to server.
    - Currently doesn't have a listener setup on express.
    - Doesn't have a DB connection.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app     = express();
 var path    = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose'); //for working with database
+var port    = process.env.PORT || 8080 // setting port
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 //set the public folder to server public assets
 app.use(express.static(__dirname + '/public'));
